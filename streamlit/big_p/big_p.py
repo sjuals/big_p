fccd87c47356f98db316cdd55fd84493f6cbc6e9
sjuals <111649953+sjuals@users.noreply.github.com>
2022-12-21 01:19:19 +0900

    Add files via upload

--- streamlit/big_p/big_p.py
+++ streamlit/big_p/big_p.py
@@ -7,6 +7,52 @@ import plotly.express as px
 
 st.set_page_config(layout='wide')
 
+import branca
+def fancy_html(row):
+    i = row
+    Number_of_2 = cc.index[i] 
+    Number_of_3 = cc['기기상태'].iloc[i]                             
+    Number_of_4 = cc['컵 잔여량'].iloc[i]  
+    Number_of_5 = cc['컵 반납용량'].iloc[i]
+    Number_of_6 = cc['범위 내 카페 수'].iloc[i] 
+
+    
+    left_col_colour = "#2A799C"
+    right_col_colour = "#C5DCE7"
+    
+    html = """<!DOCTYPE html>
+    <html>
+
+    </head>
+        <table style="height: 126px; width: 330px;">
+    <tbody>
+    <tr>
+    <td style="background-color: """+ left_col_colour +""";"><span style="color: #ffffff;">기기번호</span></td>
+    <td style="width: 160px;background-color: """+ right_col_colour +""";">{}</td>""".format(Number_of_2) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_colour +""";"><span style="color: #ffffff;">기기상태</span></td>
+    <td style="width: 160px;background-color: """+ right_col_colour +""";">{}</td>""".format(Number_of_3) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_colour +""";"><span style="color: #ffffff;">컵 잔여량</span></td>
+    <td style="width: 160px;background-color: """+ right_col_colour +""";">{}</td>""".format(Number_of_4) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_colour +""";"><span style="color: #ffffff;">컵 반납용량</span></td>
+    <td style="width: 160px;background-color: """+ right_col_colour +""";">{}</td>""".format(Number_of_5) + """
+    </tr>
+    <tr>
+    <td style="background-color: """+ left_col_colour +""";"><span style="color: #ffffff;">범위 내 카페 수</span></td>
+    <td style="width: 160px;background-color: """+ right_col_colour +""";">{}</td>""".format(Number_of_6) + """
+
+    </tr>
+    </tbody>
+    </table>
+    </html>
+    """
+    return html
+
 
 # sidebar
 with st.sidebar:
@@ -28,6 +74,9 @@ with st.sidebar:
 
     
 st.title('다회용 컵 대여·반납기 솔루션')
+    
+    
+    
     
 tab1, tab2, tab3 = st.tabs(['입지선정', '변수 분석', '측정 결과'])
 
@@ -66,25 +115,60 @@ with tab1:
 
     df = pd.read_csv('streamlit/big_p/서구 쌍촌동.csv')
     df = df[['경도', '위도', '상호명']]
+    df['카페구분'] = kmeans.labels_
 
 
     cc['기기상태'] = ['정상', '점검중', '고장', '정상', '정상']
     cc['컵 잔여량'] = [1561, 220, 3000, 231, 0]
     cc['컵 반납용량'] = [1, 0.8, 0.2, 0.1, 0]
+    cc['범위 내 카페 수'] = [35, 52, 47, 71, 32]
+
+    
     # 지도
+
+  
+    
     import folium
     from folium.plugins import MarkerCluster
 
     m = folium.Map(location=[35.152421, 126.855071], tiles='openstreetmap', zoom_start=15)
 
     for i in range(len(cc)):
-        folium.Marker([cc.iloc[i][1], cc.iloc[i][0]], popup=(cc.index[i],cc['기기상태'].iloc[i]), icon=folium.Icon(color='red')).add_to(m)
+        num_of_casualties1 = cc.index[i]
+        if num_of_casualties1 == 1:
+            color = 'blue'
+        elif num_of_casualties1 == 2:
+            color = 'green'
+        elif num_of_casualties1 == 3:
+            color = 'orange'
+        elif num_of_casualties1 == 4:
+            color = 'purple'
+        else:
+            color = 'red'
 
-    mc = MarkerCluster()
+        html = fancy_html(i)
+        iframe = branca.element.IFrame(html=html,width=350,height=150)
+        popup = folium.Popup(iframe,parse_html=True)
+        folium.Marker([cc.iloc[i][1], cc.iloc[i][0]], popup=popup, icon=folium.Icon(color=color)).add_to(m)
+
+
+
+    # mc = MarkerCluster()
     for x in range(len(df)):
-        mc.add_child(folium.Circle(location = [df.iloc[x][1], df.iloc[x][0]],popup=df['상호명'].iloc[x], radius=3))
+        num_of_casualties = df['카페구분'].iloc[x]
+        if num_of_casualties == 1:
+            color = 'blue'
+        elif num_of_casualties == 2:
+            color = 'green'
+        elif num_of_casualties == 3:
+            color = 'orange'
+        elif num_of_casualties == 4:
+            color = 'purple'
+        else:
+            color = 'red'
+        folium.Circle(location = [df.iloc[x][1], df.iloc[x][0]], popup=df['상호명'].iloc[x], color=color, radius=3).add_to(m)
 
-    m.add_child(mc)
+
 
     m.save('map.html')
 
@@ -100,15 +184,27 @@ with tab1:
 
 
 
+
     # 지도 불러오기
     import streamlit.components.v1 as components
     p = open('map.html', encoding='utf-8')
 
     st.header('서구 쌍촌동 카페 및 설치 위치')
     components.html(p.read(), width=950, height=650)
-    st.markdown('### 기기 정보')
-    cc
 
+    st.markdown('### 기기 정보')
+    if st.checkbox('기기 정보 데이터 보기'):
+        st.dataframe(cc)
+    
+    option = st.selectbox('',cc.index.unique())
+    df_selected1 = cc.loc[(cc.index.unique() == option)]
+    df_selected2 = df.loc[(df['카페구분'] == option)]
+    st.write(option, '번 기기', df_selected1)
+    st.write(option, '번 기기 범위 카페 목록', df_selected2)
+    
+    
+    
+    
 
 with tab2:
     end[['등록인구 (명)', '탑승량', '플라스틱 배출량', '카페수', '편의점수', '유동인구', '의료기관 수', '대학교 수', '지수']].astype(int)
